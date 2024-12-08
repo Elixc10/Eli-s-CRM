9edc75883b72be24f529ed9224dbe92fd0a68d6e
Francis Eleazar Cinco <cincoeleazar00@gmail.com>
2024-12-08 23:43:12 +0800

    registration

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 
 def home(request):
@@ -30,4 +30,8 @@ def logout_user (request):
 
 
 def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
 	return render(request,'register.html', {})
